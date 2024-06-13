164b2e45f6b26580c57540dc32f0aea1678f7c2e
unknown <nduflot@dawan.fr>
2024-06-13 12:54:37 +0200

    theme perso inserrer bibliothèque js

---
+++ themes/mon-theme/js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const scaleElements = document.querySelectorAll('.button');
+    
+    scaleElements.forEach(element=>{
+        element.classList.add('scaleOn');
+    })
+});
